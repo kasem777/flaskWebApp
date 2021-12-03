86c54e70d3cef9ac2cdd92499e4d4d28d98c5f9b
Kasem777 <kasem4coding@gmail.com>
2021-12-02 21:33:39 -0600

    adding BootStrap

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Corey Schafer",
+        "title": "Blog Post 1",
+        "content": "First Post",
+        "date_posted": "April 20, 2021",
+    },
+    {
+        "author": "Mo 777",
+        "title": "Blog Post 2",
+        "content": "Second Post",
+        "date_posted": "April 21, 2021",
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
